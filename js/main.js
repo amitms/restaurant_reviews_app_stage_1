47b9252a14b5a78cb54e538f7c8165d574cadf8c
amitms <amitms2005@gmail.com>
2018-09-06 23:21:54 -0400

    map container for restaurant adjusted

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'pk.eyJ1IjoiYW1pdG1zIiwiYSI6ImNqbHB3NTVjYTI5amoza3Mxd3hhNjRlMnMifQ.wFULv_xNAuiCpt07gSAQFA',
+    mapboxToken:'pk.eyJ1IjoiYW1pdG1zIiwiYSI6ImNqbHB3NTVjYTI5amoza3Mxd3hhNjRlMnMifQ.wFULv_xNAuiCpt07gSAQFA',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
